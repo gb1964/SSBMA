c38f92a7accf0872a2bd510e6502e95e135e8a2d
SBM <bg@gbdeMacBook-Pro.local>
2014-09-12 07:53:06 +0800

    Initial Commit

---
+++ SSMA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSMA
+//
+//  Created by gb on 14-9-12.
+//  Copyright (c) 2014年 com.darewaydigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
